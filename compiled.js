dbbc9ed60bc1e6521b9db53ae848b2efb28eae73
Peter Xu <peter_xu@epcnszxw0446.princeton.epam.com>
2017-11-10 21:08:45 +0800

    first commit

---
+++ compiled.js
@@ -0,0 +1,19 @@
+
+
+// Environment setup (JSX to JS)
+// https://babeljs.io/docs/plugins/transform-react-jsx/
+
+let profile = dom(
+    "div",
+    { className: "header" },
+    dom(
+        "img",
+        null,
+        "img"
+    ),
+    dom(
+        "h3",
+        null,
+        "h3"
+    )
+);
---
+++ index.js
@@ -0,0 +1,9 @@
+// Environment setup (JSX to JS)
+// https://babeljs.io/docs/plugins/transform-react-jsx/
+
+let profile = (
+    <div className="header">
+        <img>img</img>
+        <h3>h3</h3>
+    </div>
+);
