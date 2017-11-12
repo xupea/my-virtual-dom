57575f9d3c8a86fac54d06bc03f6ec9f34108c54
Peter Hsu <sysu.peter.hsu@gmail.com>
2017-11-12 23:05:38 +0800

    add bfs and dfs

--- compiled.js
+++ compiled.js
@@ -1,19 +1,117 @@
+'use strict';
 
+// import {dom} from 'deku';
 
-// Environment setup (JSX to JS)
-// https://babeljs.io/docs/plugins/transform-react-jsx/
+// let profile = (
+//     <div className="header">
+//         <img>img</img>
+//         <h3>h3</h3>
+//     </div>
+// );
 
-let profile = dom(
-    "div",
-    { className: "header" },
-    dom(
-        "img",
-        null,
-        "img"
-    ),
-    dom(
-        "h3",
-        null,
-        "h3"
-    )
-);
+var vd = {
+    tag: 'div',
+    id: '0-0',
+    children: [{
+        tag: 'span-a',
+        id: '1-0',
+        children: [{
+            tag: 'span1',
+            id: '2-0',
+            children: []
+        }, {
+            tag: 'span1',
+            id: '2-1',
+            children: []
+        }]
+    }, {
+        tag: 'span-b',
+        id: '1-1',
+        children: [{
+            tag: 'span2',
+            id: '2-2',
+            children: []
+        }]
+    }]
+};
+
+var mystack = [];
+var visited_nodes = {};
+var visited_nodes_order = [];
+
+mystack.push(vd);
+
+// DFS主要采用递归实现，依次遍历节点，如果遍历到的节点有子节点，则开始遍历子节点
+// up to down, left to right
+var DFSTraverse = function DFSTraverse() {
+
+    while (mystack.length > 0) {
+        var currentNode = mystack.pop();
+        visited_nodes_order.push(currentNode.id);
+        var _iteratorNormalCompletion = true;
+        var _didIteratorError = false;
+        var _iteratorError = undefined;
+
+        try {
+            for (var _iterator = currentNode.children.reverse()[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+                var child = _step.value;
+
+                mystack.push(child);
+            }
+        } catch (err) {
+            _didIteratorError = true;
+            _iteratorError = err;
+        } finally {
+            try {
+                if (!_iteratorNormalCompletion && _iterator.return) {
+                    _iterator.return();
+                }
+            } finally {
+                if (_didIteratorError) {
+                    throw _iteratorError;
+                }
+            }
+        }
+    }
+};
+
+// DFSTraverse();
+
+var myqueue = [];
+
+myqueue.push(vd);
+
+var BFSTraverse = function BFSTraverse() {
+    while (myqueue.length > 0) {
+        var currentNode = myqueue.shift();
+        visited_nodes_order.push(currentNode.id);
+        var _iteratorNormalCompletion2 = true;
+        var _didIteratorError2 = false;
+        var _iteratorError2 = undefined;
+
+        try {
+            for (var _iterator2 = currentNode.children[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+                var child = _step2.value;
+
+                myqueue.push(child);
+            }
+        } catch (err) {
+            _didIteratorError2 = true;
+            _iteratorError2 = err;
+        } finally {
+            try {
+                if (!_iteratorNormalCompletion2 && _iterator2.return) {
+                    _iterator2.return();
+                }
+            } finally {
+                if (_didIteratorError2) {
+                    throw _iteratorError2;
+                }
+            }
+        }
+    }
+};
+
+BFSTraverse();
+
+console.log(visited_nodes_order);
--- index.js
+++ index.js
@@ -1,9 +1,74 @@
-// Environment setup (JSX to JS)
-// https://babeljs.io/docs/plugins/transform-react-jsx/
+const vd = {
+    tag: 'div',
+    id: '0-0',
+    children: [
+        {
+            tag: 'span-a',
+            id: '1-0',
+            children: [
+                {
+                    tag: 'span1',
+                    id: '2-0',
+                    children: [] 
+                },
+                {
+                    tag: 'span1',
+                    id: '2-1',
+                    children: [] 
+                }
+            ]
+        },
+        {
+            tag: 'span-b',
+            id: '1-1',
+            children: [
+                {
+                    tag: 'span2',
+                    id: '2-2',
+                    children: [] 
+                }
+            ]
+        }
+    ] 
+};
 
-let profile = (
-    <div className="header">
-        <img>img</img>
-        <h3>h3</h3>
-    </div>
-);
+let mystack = [];
+let visited_nodes = {};
+let visited_nodes_order = [];
+
+mystack.push(vd);
+
+// DFS主要采用递归实现，依次遍历节点，如果遍历到的节点有子节点，则开始遍历子节点
+// up to down, left to right
+const DFSTraverse = function() {
+
+    while(mystack.length > 0) {
+        let currentNode = mystack.pop();
+        visited_nodes_order.push(currentNode.id);
+        for (let child of currentNode.children.reverse()) {
+            mystack.push(child);
+        }
+    }
+
+}
+
+// DFSTraverse();
+
+// BFS采用队列的思想，采用出队的方式遍历节点，如果遍历到的节点有子节点，则将子节点入队
+
+let myqueue = [];
+myqueue.push(vd);
+
+const BFSTraverse = function() {
+    while(myqueue.length > 0) {
+        let currentNode = myqueue.shift();
+        visited_nodes_order.push(currentNode.id);
+        for(let child of currentNode.children) {
+            myqueue.push(child);
+        }
+    }
+}
+
+BFSTraverse();
+
+console.log(visited_nodes_order);
